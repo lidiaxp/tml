03e5c6446f6ea13f886ba8ece768cf07e4ae95ab
Lidia Gianne <lidiagianne@gmail.com>
2019-05-13 02:24:43 -0300

    Update environment.js

--- dist/common/environment.js
+++ dist/common/environment.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
-    server: { port: process.env.SERVER_PORT || 3000 },
+    server: { port: process.env.PORT || 5000 },
     db: { url: process.env.MONGOLAB_URI},
     security: { saltRounds: process.env.SALT_ROUNDS || 10 },
 };
