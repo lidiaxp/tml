9733aa0f4b7dcdd7ac30430ea65db16a9477c76d
lidiaxp <lidiagianne@gmail.com>
2019-05-13 02:21:12 -0300

    first commit

---
+++ model/usuario.model.ts
@@ -0,0 +1,226 @@
+import * as mongoose from 'mongoose'
+import {validateCPF} from '../common/validators'
+import * as bcrypt from 'bcrypt'
+import {environment} from '../common/environment'
+import{Profissional} from './profissional.model'
+import{Salao} from './salao.model'
+
+
+
+
+
+export interface EnderecoItem extends mongoose.Document{
+  estado: String,
+  cidade: String,
+  cep: String,
+  numero: String
+  complemento: String
+}
+
+
+
+export interface ContatosItem extends mongoose.Document{
+  email:String,
+  celular: String,
+  instagram: String,
+  facebook: String
+}
+
+export interface AvaliacaoItem extends mongoose.Document{
+  avaliador: mongoose.Types.ObjectId | Usuario,
+  nota: Number,
+  comentario: String
+}
+
+export interface FotosItem extends mongoose.Document{
+  tag: String,
+  imagem: String
+}
+
+export interface PreferidoItem extends mongoose.Document{
+  cliente: mongoose.Types.ObjectId | Usuario
+}
+
+
+export interface Usuario extends mongoose.Document{
+  nome: String,
+  rg: String,
+  cpf: String,
+  codigo: String,
+  nascimento: String,
+  contatos: ContatosItem[],
+  endereco: EnderecoItem[],
+  fotos: FotosItem[],
+  avaliacao: AvaliacaoItem[],
+  preferido: PreferidoItem[],
+  perfilProfissional: mongoose.Types.ObjectId | Profissional,
+  perfilSalao: mongoose.Types.ObjectId | Salao
+}
+
+// export interface UsuarioModel extends mongoose.Model<Usuario>{
+//   findByPreferido(preferido:mongoose.Types.ObjectId | Usuario): Promise<Usuario>
+// }
+
+const enderecoSchema = new mongoose.Schema({
+  estado:{
+    type: String,
+    required:true,
+    enum:['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+  },
+  cidade:{
+    type: String,
+    required: true
+  },
+  cep:{
+    type: String,
+    required: true,
+    maxlength:8
+  },
+  numero:{
+    type: String,
+    required: true,
+    maxlength: 5
+  },
+  complemento:{
+    type: String,
+    required: true,
+    maxlength: 25
+  }
+})
+
+const contatosSchema = new mongoose.Schema({
+  email:{
+    type:String,
+    unique: true,
+    required: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  },
+  celular:{
+    type: String,
+    unique: true,
+    required: true
+  },
+  instagram:{
+    type: String,
+    unique: true,
+    required: true
+  },
+  facebook:{
+    type: String,
+    unique: true,
+    required: true
+  }
+})
+
+const avaliacaoSchema = new mongoose.Schema({
+  avaliador:{
+    type:mongoose.Schema.Types.ObjectId,
+    required: false,
+    selec: false
+  },
+  nota:{
+    type: Number,
+    required: false,
+    
+  },
+  comentario:{
+    type: String,
+    required: false,
+    maxlength: 120
+  }
+})
+
+const fotosSchema = new mongoose.Schema({
+  tag:{
+    type: String,
+    required: false,
+    select: false
+  },
+  imagem:{
+    type: String,
+    required: false,
+    select: false
+  }
+})
+
+const preferidoSchema = new mongoose.Schema({
+  cliente:{
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+    select: false
+  }
+})
+
+const usuarioSchema = new mongoose.Schema({
+  nome:{
+    type: String,
+    required: true,
+    maxlength: 40,
+    minlength: 1
+  },
+  rg:{
+    type:String,
+    required: true,
+    unique: true
+  },
+  cpf:{
+    type: String,
+    require: false,
+    validate:{
+      validator: validateCPF,
+      message:'{PATH}: Invalid CPF({VALUE})'
+    }
+  },
+  codigo:{
+    type:String,
+    required: true,
+    unique: true,
+    maxlength: 10,
+    minlength: 2
+  },
+  nascimento:{
+    type: String,
+    required: true
+  },
+  contatos:{
+    type:[contatosSchema],
+    required: false,
+    select: false
+  },
+  endereco:{
+    type:[enderecoSchema],
+    required: false,
+    select: false
+  },
+  fotos:{
+    type:[fotosSchema],
+    required: false,
+    select: false
+  },
+  avaliacao:{
+    type:[avaliacaoSchema],
+    required: false,
+    select: false
+  },
+  preferido:{
+    type:[preferidoSchema],
+    required: false,
+    select: false
+  },
+  perfilProfissional:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Profissao',
+    required: false
+  },
+  perfilSalao:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Salao',
+    required: false
+  }
+})
+
+// usuarioSchema.static.findByPreferido = function(preferido: mongoose.Schema.Types.ObjectId){
+//   return this.findOne({preferido})
+// }
+
+export const Usuario = mongoose.model<Usuario>('Usuario', usuarioSchema)
---
+++ routers/usuarios.router.ts
@@ -0,0 +1,169 @@
+import * as mongoose from 'mongoose'
+import{ModelRouter} from '../common/model-router'
+import * as restify from 'restify'
+import{NotFoundError} from 'restify-errors'
+import{Usuario} from '../model/usuario.model'
+
+class UsuarioRouter extends ModelRouter<Usuario> {
+
+  constructor(){
+    super(Usuario)
+  }
+
+
+
+  
+
+
+
+
+
+
+  findContatos = (req,resp,next)=>{
+    Usuario.findById(req.params.id, "+contatos").then(cont=>{
+      if(!cont){
+        throw new NotFoundError('Usuario não encontrado')
+      }else{
+        resp.json(cont.contatos)
+        return next()
+      }
+    }).catch(next)
+  }
+  replaceContatos = (req,resp,next)=>{
+  Usuario.findById(req.params.id).then(rest=>{
+    if(!rest){
+      throw new NotFoundError('Usuario não encontrado')
+    }else{
+      rest.contatos = req.body // um array
+      return rest.save()
+    }
+  }).then(rest=>{
+    resp.json(rest.contatos)
+    return next()
+  }).catch(next)
+}
+
+findEndereco = (req,resp,next)=>{
+  Usuario.findById(req.params.id, "+endereco").then(cont=>{
+    if(!cont){
+      throw new NotFoundError('Usuario não encontrado')
+    }else{
+      resp.json(cont.endereco)
+      return next()
+    }
+  }).catch(next)
+}
+replaceEndereco = (req,resp,next)=>{
+Usuario.findById(req.params.id).then(usu=>{
+  if(!usu){
+    throw new NotFoundError('Usuario não encontrado')
+  }else{
+    usu.endereco = req.body // um array
+    return usu.save()
+  }
+}).then(usu=>{
+  resp.json(usu.endereco)
+  return next()
+}).catch(next)
+}
+
+findAvaliacao = (req,resp,next)=>{
+  Usuario.findById(req.params.id, "+avaliacao").then(ava=>{
+    if(!ava){
+      throw new NotFoundError('Sem avaliação')
+    }else{
+      resp.json(ava.avaliacao)
+      return next()
+    }
+  }).catch(next)
+}
+replaceAvaliacao = (req,resp,next)=>{
+Usuario.findById(req.params.id).then(ava=>{
+  if(!ava){
+    throw new NotFoundError('Sem Avaliação')
+  }else{
+    ava.fotos = req.body // um array
+    return ava.save()
+  }
+}).then(ava=>{
+  resp.json(ava.avaliacao)
+  return next()
+}).catch(next)
+}
+
+findFotos = (req,resp,next)=>{
+  Usuario.findById(req.params.id, "+fotos").then(fot=>{
+    if(!fot){
+      throw new NotFoundError('Usuario não encontrado')
+    }else{
+      resp.json(fot.fotos)
+      return next()
+    }
+  }).catch(next)
+}
+replaceFotos = (req,resp,next)=>{
+Usuario.findById(req.params.id).then(fot=>{
+  if(!fot){
+    throw new NotFoundError('Usuario não encontrado')
+  }else{
+    fot.fotos = req.body // um array
+    return fot.save()
+  }
+}).then(fot=>{
+  resp.json(fot.endereco)
+  return next()
+}).catch(next)
+}
+
+findById = (req,resp,next)=>{
+  this.model.findById(req.params.id)
+  .populate('Salao', 'espaco, salas, comentarios, precoHora, kit')
+  .populate('Profissao', 'servico, tipo, comentario')
+  .then(this.render(resp,next))
+  .catch(next)
+}
+
+/*findByPreferido = (req,resp,next)=>{ // problema de versão. Não estou conseguindo usar a rota de procurar primeiro por email
+  if(req.query.preferido){
+    Usuario.findByPreferido(req.query.preferido)
+    .then(this.renderAll(resp, next))
+    .catch(next)
+  }else{
+    next()
+  }
+
+}*/
+
+
+
+  applyRoutes(application: restify.Server){
+
+    
+   
+    application.get('/usuario',this.findAll)
+    application.get('/usuario/:id',[this.validateId, this.findById])
+    application.post('/usuario', this.save)
+    application.put('/usuario/:id',[this.validateId, this.replace])
+    application.patch('/usuario/:id',[this.validateId, this.update])
+    application.del('/usuario/:id',[this.validateId, this.delete])
+
+    application.get('/usuario/:id/contatos', [this.validateId, this.findContatos])
+    application.put('/usuario/:id/contatos', [this.validateId, this.replaceContatos])
+
+    application.get('/usuario/:id/endereco',[this.validateId,  this.findEndereco])
+    application.put('/usuario/:id/endereco', [this.validateId, this.replaceEndereco])
+
+    application.get('/usuario/:id/avaliacao',[this.validateId,  this.findAvaliacao])
+    application.put('/usuario/:id/avaliacao', [this.validateId, this.replaceAvaliacao])
+
+    application.get('/usuario/:id/fotos', [this.validateId, this.findFotos])
+    application.put('/usuario/:id/fotos', [this.validateId, this.replaceFotos])
+
+    
+
+
+
+  }
+}
+
+export const usuarioRouter = new UsuarioRouter()
