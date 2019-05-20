dede4b4444f3d09b1064e610ffe4ca6ee3c19951
Lidia Gianne <lidiagianne@gmail.com>
2019-05-20 15:46:04 -0300

    Update model-router.js

--- dist/common/model-router.js
+++ dist/common/model-router.js
@@ -9,7 +9,7 @@ class ModelRouter extends router_1.Router {
         super();
         this.model = model;
         this.validateId = (req, resp, next) => {
-            if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            if (mongoose.Types.ObjectId.isValid(req.params.id)) {
                 next(new restify_errors_1.NotFoundError('Document not found'));
             }
             else {
