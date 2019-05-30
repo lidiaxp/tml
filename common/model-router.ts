df0abb49c112e10b6e6ba8abc6b9f843ea225aae
lidiaxp <lidiagianne@gmail.com>
2019-05-30 16:30:24 -0300

    query

--- common/model-router.ts
+++ common/model-router.ts
@@ -33,7 +33,7 @@ this.model.find()
   }
 
   find = (req,resp,next)=>{
-    this.model.find({descricao:{$search: '' + req.query.descricao}}) 
+    this.model.find({descricao:{$text: { $search: req.params.descricao }}}) 
         .then(this.renderAll(resp,next))
         .catch(next)
       }
