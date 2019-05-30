0f7618d63c677b67f5eae5827285ab28c99b06d8
lidiaxp <lidiagianne@gmail.com>
2019-05-30 17:50:18 -0300

    query

--- common/model-router.ts
+++ common/model-router.ts
@@ -34,7 +34,7 @@ this.model.find()
 
   find = (req,resp,next)=>{
     var search = 'qwert';
-    this.model.find({descricao: { $regex: req.params.descricao, $options: 'i' }}) 
+    this.model.find({descricao: { $regex: RegExp(/w/, i)}}) 
         .then(this.renderAll(resp,next))
         .catch(next)
       }
