7b14ceda343107a315887d25bf604497acdf7dc7
yuripnp <yuripnp1992@gmail.com>
2019-06-14 12:45:10 -0300

    colocando o hasAny

--- security/authz.handler.ts
+++ security/authz.handler.ts
@@ -5,7 +5,7 @@ import {ForbiddenError} from 'restify-errors';
 // ela vai receber os perfils e retornar um requesthendler
 export const authorized: (...profiles: string[])=> restify.RequestHandler =(...profiles)=>{
     return (req,resp,next)=>{
-        if(req.params.authenticated !== undefined /*&& req.authenticated.hasAny(...profiles)/** */){
+        if(req.params.authenticated !== undefined && req.authenticated.hasAny(...profiles)){
             next()
         }else{
             next(new ForbiddenError('Permissão Negada'))
